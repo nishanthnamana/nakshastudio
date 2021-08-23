1b914e5f7561671b3dffb2550185f4605d17c027
Nishanth Namana <nishanth.namana@gmail.com>
2021-08-22 19:12:20 -0500

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,29 @@
+$(function () {
+  $(".owl-carousel").owlCarousel({
+    items: 3,
+    autoplay: true,
+    smartSpeed: 800,
+    loop: true,
+    autoplayHoverPause: true,
+    autoplaySpeed: 900,
+    dots: true,
+    responsive: {
+      // breakpoint from 0 up
+      0: {
+        items: 1,
+      },
+      // breakpoint from 480 up
+      480: {
+        items: 2,
+      },
+      // breakpoint from 768 up
+      768: {
+        items: 3,
+      },
+      // breakpoint from 768 up
+      991: {
+        items: 5,
+      },
+    },
+  });
+});
