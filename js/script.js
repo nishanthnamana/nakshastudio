4ec316ae39da2d41d4a5939ece3c2611d1c959b8
Nishanth Namana <nishanth.namana@gmail.com>
2021-09-01 08:51:21 -0500

    Added Carousels

--- js/script.js
+++ js/script.js
@@ -1,9 +1,72 @@
-const navLinks = document.getElementById("navLinks");
+var $slider = $(".slideshow .slider"),
+  maxItems = $(".item", $slider).length,
+  dragging = false,
+  tracking,
+  rightTracking;
 
-function showMenu() {
-  navLinks.style.right = "0";
+$sliderRight = $(".slideshow")
+  .clone()
+  .addClass("slideshow-right")
+  .appendTo($(".split-slideshow"));
+
+rightItems = $(".item", $sliderRight).toArray();
+reverseItems = rightItems.reverse();
+$(".slider", $sliderRight).html("");
+for (i = 0; i < maxItems; i++) {
+  $(reverseItems[i]).appendTo($(".slider", $sliderRight));
 }
 
-function hideMenu() {
-  navLinks.style.right = "-200px";
-}
+$slider.addClass("slideshow-left");
+$(".slideshow-left")
+  .slick({
+    vertical: true,
+    verticalSwiping: true,
+    arrows: false,
+    infinite: true,
+    dots: true,
+    speed: 1000,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    cssEase: "cubic-bezier(0.7, 0, 0.3, 1)",
+  })
+  .on("beforeChange", function (event, slick, currentSlide, nextSlide) {
+    if (
+      currentSlide > nextSlide &&
+      nextSlide == 0 &&
+      currentSlide == maxItems - 1
+    ) {
+      $(".slideshow-right .slider").slick("slickGoTo", -1);
+      $(".slideshow-text").slick("slickGoTo", maxItems);
+    } else if (
+      currentSlide < nextSlide &&
+      currentSlide == 0 &&
+      nextSlide == maxItems - 1
+    ) {
+      $(".slideshow-right .slider").slick("slickGoTo", maxItems);
+      $(".slideshow-text").slick("slickGoTo", -1);
+    } else {
+      $(".slideshow-right .slider").slick(
+        "slickGoTo",
+        maxItems - 1 - nextSlide
+      );
+      $(".slideshow-text").slick("slickGoTo", nextSlide);
+    }
+  });
+
+$(".slideshow-right .slider").slick({
+  swipe: false,
+  vertical: true,
+  arrows: false,
+  infinite: true,
+  speed: 950,
+  cssEase: "cubic-bezier(0.7, 0, 0.3, 1)",
+  initialSlide: maxItems - 1,
+});
+$(".slideshow-text").slick({
+  swipe: false,
+  vertical: true,
+  arrows: false,
+  infinite: true,
+  speed: 900,
+  cssEase: "cubic-bezier(0.7, 0, 0.3, 1)",
+});
